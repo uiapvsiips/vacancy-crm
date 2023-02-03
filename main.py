1d1f83f0ac2219f32b7fdfcae59b50ef31e34f8c
kyrylo <uiapvsiips@gmail.com>
2023-02-03 21:45:46 +0200

    hw2

--- main.py
+++ main.py
@@ -1,69 +1,70 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
-app.run()
 list_of_vacancies = [
     {
-     "id": 1,
-     "creation_date": "20.01.2023", "status": 1,
-     "company": "Some company2",
-     "contacts_ids": [5,6],
-     "description": "Vacancy description",
-     "position_name": "Middle Python dev",
-     "comment": "Good vacancy and company",
-     "user_id": 1
-     },
-     {
-     "id": 2,
-     "creation_date": "28.01.2023", "status": 1,
-     "company": "Some company3",
-     "contacts_ids": [5,6],
-     "description": "Vacancy description",
-     "position_name": "Middle Python dev",
-     "comment": "Good vacancy, bad company",
-     "user_id": 1
-     },
-     {
-     "id": 3,
-     "creation_date": "23.01.2023", "status": 1,
-     "company": "Some company4",
-     "contacts_ids": [5,6],
-     "description": "Vacancy description",
-     "position_name": "Trainee Python dev",
-     "comment": "Bad vacancy and company",
-     "user_id": 1
-     }
+        "id": 1,
+        "creation_date": "20.01.2023", "status": 1,
+        "company": "Some company2",
+        "contacts_ids": [5, 6],
+        "description": "Vacancy description",
+        "position_name": "Middle Python dev",
+        "comment": "Good vacancy and company",
+        "user_id": 1
+    },
+    {
+        "id": 2,
+        "creation_date": "28.01.2023", "status": 1,
+        "company": "Some company3",
+        "contacts_ids": [5, 6],
+        "description": "Vacancy description",
+        "position_name": "Middle Python dev",
+        "comment": "Good vacancy, bad company",
+        "user_id": 1
+    },
+    {
+        "id": 3,
+        "creation_date": "23.01.2023", "status": 1,
+        "company": "Some company4",
+        "contacts_ids": [5, 6],
+        "description": "Vacancy description",
+        "position_name": "Trainee Python dev",
+        "comment": "Bad vacancy and company",
+        "user_id": 1
+    }
 ]
 
 list_of_events = [
     {
-    "id":1,
-    "vacancy_id": 1,
-    "description": "Some description",
-    "event_date": "01.01.2023",
-    "title": "Event title vacancy 3",
-    "due_to_date": "01.02.2023",
-    "status": 1
+        "id": 1,
+        "vacancy_id": 1,
+        "description": "Some description",
+        "event_date": "01.01.2023",
+        "title": "Event title vacancy 3",
+        "due_to_date": "01.02.2023",
+        "status": 1
     },
     {
-    "id":2,
-    "vacancy_id": 2,
-    "description": "Some description",
-    "event_date": "29.01.2023",
-    "title": "Event title vacancy 3",
-    "due_to_date": "03.02.2023",
-    "status": 1
+        "id": 2,
+        "vacancy_id": 2,
+        "description": "Some description",
+        "event_date": "29.01.2023",
+        "title": "Event title vacancy 3",
+        "due_to_date": "03.02.2023",
+        "status": 1
     },
     {
-    "id":3,
-    "vacancy_id": 2,
-    "description": "Some description",
-    "event_date": "30.01.2023",
-    "title": "Event title vacancy 3",
-    "due_to_date": "05.02.2023",
-    "status": 1
+        "id": 3,
+        "vacancy_id": 2,
+        "description": "Some description",
+        "event_date": "30.01.2023",
+        "title": "Event title vacancy 3",
+        "due_to_date": "05.02.2023",
+        "status": 1
     },
 ]
+
+
 @app.route('/', methods=['GET'])
 @app.route('/user/', methods=['GET'])
 def main_page():
@@ -102,7 +103,7 @@ def get_user_vacancies_history():
 
 @app.get('/vacancy/')
 def get_user_vacancies():
-    return "Get your vacancies!"
+    return list_of_vacancies
 
 
 @app.post('/vacancy/')
@@ -112,8 +113,8 @@ def post_new_user_vacancies():
 
 @app.get('/vacancy/<int:vacancy_id>/')
 def get_user_vacancy_by_id(vacancy_id=None):
-
-    return f"Get your vacancie {vacancy_id}!"
+    return next((vacancy for vacancy in list_of_vacancies if vacancy['id'] == vacancy_id),
+                f'No vacancies with vacancy_id: {vacancy_id}')
 
 
 @app.put('/vacancy/<int:vacancy_id>/')
@@ -123,7 +124,7 @@ def update_some_vacancy(vacancy_id=None):
 
 @app.get('/vacancy/<int:vacancy_id>/events/')
 def get_user_events(vacancy_id=None):
-    return f"Get events for vacancy {vacancy_id}!"
+    return list((event for event in list_of_events if event['vacancy_id'] == vacancy_id))
 
 
 @app.post('/vacancy/<int:vacancy_id>/events/')
@@ -133,7 +134,9 @@ def post_new_event_for_vacancy(vacancy_id=None):
 
 @app.get('/vacancy/<int:vacancy_id>/events/<event_id>/')
 def get_event_for_vacancy_by_id(vacancy_id=None, event_id=None):
-    return f"Get event {event_id} for vacancy {vacancy_id}!"
+    return next(
+        (event for event in list_of_events if event['id'] == int(event_id) and event['vacancy_id'] == vacancy_id),
+        f'No events with event_id: {event_id}')
 
 
 @app.put('/vacancy/<int:id>/events/<int:event_id>/')
